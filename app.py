e0664e126e79175abca1ba796d91aa7404569ac1
tanojrahul <tanojrahul19@gmail.com>
2024-11-30 16:17:34 +0530

    initial commit

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource, reqparse, marshal, abort, fields, marshal_with
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] ="postgresql://postgres.efnrejuwcbrarhvjddcv:Tanoj%40190605@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db=SQLAlchemy(app)
@@ -20,11 +21,41 @@ user_fields = {
     'email': fields.String
 }
 
+class user_edit(Resource):
+    @marshal_with(user_fields)
+    def get(self,user_id):
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            abort(404,message='User not found')
+        return user, 200
+
+    @marshal_with(user_fields)
+    def put(self,user_id):
+        args = user_args.parse_args()
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            abort(404,message='User not found')
+        user.name = args['name']
+        user.email = args['email']
+        db.session.commit()
+        return user, 200
+
+    def delete(self,user_id):
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            abort(404,message='User not found')
+        db.session.delete(user)
+        db.session.commit()
+        return 'Successfully deleted user', 204
+
 class user(Resource):
     @marshal_with(user_fields)
     def post(self):
         args = user_args.parse_args()
         user = User(name=args['name'],email=args['email'])
+        if User.query.filter_by(email=args['email']).first():
+            abort(409,message='User already exists')
+
         db.session.add(user)
         db.session.commit()
         return user, 201
@@ -36,6 +67,7 @@ class user(Resource):
 
 
 api.add_resource(user,'/user')
+api.add_resource(user_edit,'/user/<int:user_id>')
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
